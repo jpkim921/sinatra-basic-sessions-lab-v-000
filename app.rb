99029214c97745ba3d6b202d16365cb69b50064f
Jayon Kim <jpkim921@gmail.com>
2018-05-27 01:48:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,9 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  configure do
+    enable :sessions
+    set :session_secret, "peaches"
+  end
+
   get '/' do
 
     erb :index
   end
-  
+
 end
